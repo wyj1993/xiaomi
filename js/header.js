e4ae9df5d0e42b0d9c8c4a88a0e74efd9748e7bd
wyj1993 <1959617212@qq.com>
2019-01-26 10:38:46 +0800

    create project xiaomi

---
+++ js/header.js
@@ -0,0 +1,46 @@
+$(".top_cart").hover(
+    function(){
+        $(this).find("div").slideDown();
+     },
+     function(){
+         
+         $(this).find("div").slideUp();
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
